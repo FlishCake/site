45b7361e08772a729f740996b5179111789eed11
FlishCake <Flisher4@gmail.com>
2020-06-14 13:09:28 +0300

    delete article

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,11 +7,11 @@ export default ({ children }) => (
     <>
         <Header />
     
-        <Article>
+        <div>
             <Section>
                 {children}
             </Section>
-        </Article>
+        </div>
     
         <Footer />
     </>
