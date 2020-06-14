213231214c970a942068f32f6c96dd5998028166
FlishCake <Flisher4@gmail.com>
2020-06-14 10:30:53 +0300

    edit layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,13 +4,13 @@ import Header from "./header";
 import Footer from "./footer";
 
 export default ({ children }) => (
-  <>
     <Header />
+    
     <Article>
         <Section>
             {children}
         </Section>
     </Article>
+    
     <Footer />
-  </>
 );
