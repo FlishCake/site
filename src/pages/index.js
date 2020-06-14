ac23097ddf0e78208b095f88bf8e5a19b4db489d
FlishCake <Flisher4@gmail.com>
2020-06-14 21:34:22 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from "../components/layout";
 
 const Title = "Главная";
 
-const Content = props => {
+const Content = props => (
     <h1>Test</h1>
-};
+);
 
 export default () => (
   <Layout headerText={Title} content={Content} />
