4da3465e7663e2ab8f95dd1dad5b6a68124cff5f
FlishCake <Flisher4@gmail.com>
2020-06-15 19:18:20 +0300

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,36 @@ import Layout from "../components/layout";
 const Title = "Главная";
 
 const Content = (
-    <div class="content">
-        <h1>Проекты</h1>
+    <>
+        <div class="content">
+            <h1>Проекты</h1>
         
-        <div>
-            <h2>Проект</h2>
-            <p>Описание проекта</p>
+            <div>
+                <h2>Проект</h2>
+                <p>Описание проекта</p>
+            </div>
+        
+            <div>
+                <h2>Проект</h2>
+                <p>Описание проекта</p>
+            </div>
+        
         </div>
-        
-        <div>
-            <h2>Проект</h2>
-            <p>Описание проекта</p>
-        </div>
-        
-    </div>
     
-    <div class="content">
-        <h1>Записи</h1>
+        <div class="content">
+            <h1>Записи</h1>
         
-        <div>
-            <h2>Записть</h2>
-            <p>Краткое описание</p>
-        </div>
+            <div>
+                <h2>Записть</h2>
+                <p>Краткое описание</p>
+            </div>
         
-        <div>
-            <h2>Запись</h2>
-            <p>Краткое описание</p>
+            <div>
+                <h2>Запись</h2>
+                <p>Краткое описание</p>
+            </div>
         </div>
-    </div>
+    </>
 );
 
 export default () => (
--- src/pages/plans.js
+++ src/pages/plans.js
@@ -5,12 +5,14 @@ import Layout from "../components/layout";
 const Title = "Луховицы L324 План";
 
 const Content = (
-    <div class="content">
+    <>
+        <div class="content">
         
-    </div>
-    <div class="content">
-        <h1>Основные</h1>
-    </div>
+        </div>
+        <div class="content">
+            <h1>Основные</h1>
+        </div>
+    </>
 );
 
 export default () => (
