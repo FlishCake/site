84c2d35c3dc263ba9ee8f9cbab711b8633ce2ef4
FlishCake <Flisher4@gmail.com>
2020-06-17 22:02:52 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 
-import styleHomePage from "../../styles/homePage.css";
+import styleHomePage from "../styles/homePage.css";
 
 const Title = "Главная";
 
