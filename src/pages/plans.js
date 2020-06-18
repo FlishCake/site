ff0e37a871178d62c845964b2ad73dc491784049
FlishCake <Flisher4@gmail.com>
2020-06-18 21:05:10 +0300

    edit

--- src/pages/plans.js
+++ src/pages/plans.js
@@ -2,17 +2,26 @@ import React from "react";
 
 import Layout from "../components/layout";
 
+import stylePlans from "../styles/plans.css";
+
 const Title = "Луховицы L324 План";
 
 const Content = (
-    <>
+    <div className={stylePlans}>
         <div class="content">
-        
+            <div class="flex">
+                <p>25%</p>
+            </div>
         </div>
         <div class="content">
             <h1>Основные</h1>
+            <div class="flex">
+                <p class="firstItem">100%</p>
+                <h2 class="otherItem">Товарный оборот</h2>
+                <p class="otherItem">102500/90500</p>
+            </div>
         </div>
-    </>
+    </div>
 );
 
 export default () => (
