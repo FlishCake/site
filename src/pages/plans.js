27c177ad784e43615fbc9e9b99ebce292a4e6630
FlishCake <Flisher4@gmail.com>
2020-06-18 21:17:29 +0300

    edit

--- src/pages/plans.js
+++ src/pages/plans.js
@@ -8,19 +8,37 @@ const Title = "Луховицы L324 План";
 
 const Content = (
     <div className={stylePlans}>
+    
         <div class="content">
             <div class="flex">
                 <p>25%</p>
             </div>
         </div>
+        
         <div class="content">
+        
             <h1>Основные</h1>
+            
             <div class="flex">
                 <p class="firstItem">100%</p>
                 <h2 class="otherItem">Товарный оборот</h2>
                 <p class="otherItem">102500/90500</p>
             </div>
+            
+            <div class="flex">
+            
+                <div class="otherItem">
+                    <p></p>
+                    <p></p>
+                </div>
+                
+                <div class="otherItem">
+                
+                </div>
+            
+            </div>
         </div>
+        
     </div>
 );
 
