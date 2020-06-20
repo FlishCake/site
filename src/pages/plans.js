23e7588b6bc1c9c5eed8f707af89846de801bfa1
FlishCake <Flisher4@gmail.com>
2020-06-20 20:37:03 +0300

    edit

--- src/pages/plans.js
+++ src/pages/plans.js
@@ -4,9 +4,32 @@ import Layout from "../components/layout";
 
 const Title = "Луховицы L324 План";
 
+const Plan = props => (
+    <>
+        <div class="flex">
+            <p class="item">{props.percent}</p>
+            <h2 class="item">{props.name}</h2>
+            <p class="item">{props.performance}</p>
+        </div>
+                
+        <div class="flex">
+            <div class="item">
+                <p>В день: 10000</p>
+            </div>
+                    
+            <div class="item">
+                <p>До 100%: 20000</p>
+            </div>
+                    
+            <div class="item">
+                <p>Осталось: 100000</p>
+            </div>
+        </div>
+    </>
+)
+
 const Content = (
     <>
-    
         <div class="content">
             <div class="flex">
                 <p>25%</p>
@@ -16,27 +39,12 @@ const Content = (
         <div class="content plans">
             <h1>Основные</h1>
             
-            <div class="flex">
-                <p class="item">100%</p>
-                <h2 class="item">Товарный оборот</h2>
-                <p class="item">102500/90500</p>
-            </div>
-            
-            <div class="flex">
-                <div class="item">
-                    <p>В день: 10000</p>
-                </div>
-                
-                <div class="item">
-                    <p>До 100%: 20000</p>
-                </div>
-                
-                <div class="item">
-                    <p>Осталось: 100000</p>
-                </div>
-            </div>
+            <Plan
+                percent="100%"
+                name="Товарный оборот"
+                performance="150000/120000"
+            />
         </div>
-        
     </>
 );
 
