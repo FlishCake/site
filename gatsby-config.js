1b098c65583163b5953f76d190292f8186f847af
FlishCake <flishcake@ya.ru>
2020-06-21 10:09:35 +0300

    add loadscript plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+      {
+      resolve: 'gatsby-plugin-load-script',
+      options: {
+        disable: !process.env.SENTRY_DSN, // When do you want to disable it ?
+        src: 'https://browser.sentry-cdn.com/5.15.4/bundle.min.js',
+        integrity:
+          'sha384-Nrg+xiw+qRl3grVrxJtWazjeZmUwoSt0FAVsbthlJ5OMpx0G08bqIq3b/v0hPjhB',
+        crossorigin: 'anonymous',
+        onLoad: `() => Sentry.init({dsn:"${process.env.SENTRY_DSN}"})`,
+      },
+  ],
 }
